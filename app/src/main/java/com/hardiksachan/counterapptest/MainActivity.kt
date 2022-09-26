bb73f131c3abba606d3d704339d2c0219b2d65eb
hardiksachan <sachanhardik@gmail.com>
2022-09-26 16:30:22 +0530

    starting point

---
+++ app/src/main/java/com/hardiksachan/counterapptest/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.hardiksachan.counterapptest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hardiksachan.counterapptest.ui.theme.CounterAppTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CounterAppTestTheme {
+                Text("Hello, World")
+            }
+        }
+    }
+}
